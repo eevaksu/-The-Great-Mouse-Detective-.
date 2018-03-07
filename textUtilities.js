f06534ddda8df9e0416f79406023264535b1496d
eevaksu <37085446+eevaksu@users.noreply.github.com>
2018-03-07 11:19:34 +0000

    Update textUtilities.js

--- textUtilities.js
+++ textUtilities.js
@@ -1,6 +1,9 @@
 var expect = require('chai').expect;
 expect(true).to.be.true;
 function titleCase (title) {
+  var words = title.split('');
+  var titleCasedWords = words;
+  return titleCasedWords.join('');
   return title[0].toUpperCase() + title.substring(1);
 }
 expect(titleCase('the great mouse detective').to.be.a('string');
