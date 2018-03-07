5fb9097f0fa6f389f08f64fc25e3432f607f92f3
eevaksu <37085446+eevaksu@users.noreply.github.com>
2018-03-07 11:10:44 +0000

    Update textUtilities.js

--- textUtilities.js
+++ textUtilities.js
@@ -4,5 +4,6 @@ function titleCase (title) {
   return title;
 }
 expect(titleCase('the great mouse detective').to.be.a('string');
-expect (titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
 expect(titleCase('a')).to.equal('A');
+expect(titleCase('vertigo)).to.equal('Vertigo');
+expect (titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
